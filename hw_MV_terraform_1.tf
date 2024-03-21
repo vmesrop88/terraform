156981c6e22f52ec5a4b94f254ce6638d897a088
vmesrop88 <159133021+vmesrop88@users.noreply.github.com>
2024-03-22 00:24:08 +0400

    Add files via upload

---
+++ hw_MV_terraform_1.tf
@@ -0,0 +1,44 @@
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "mesropv2024"
+  acl = "private"
+
+}
+
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "TfTable"
+  hash_key       = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "mesropv2024"
+    key            = "terraform/state"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "TfTable"
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  count         = 3
+  ami           = "ami-0e0bf53f6def86294" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "MyEc2-${count.index + 1}"
+  }
+}
